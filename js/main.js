bdb71f77618e755559c50f4bb5185c124c9de659
ahmed-samir <ahmedsamirr126@gmail.com>
2024-05-18 16:52:11 +0200

    show and hide the notifications

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
- 
+//  get nav elements
+let navSvg = document.querySelector(".prs-icon")
+let navSvgSec = document.querySelector(".prs-icon-2")
+let notifications = document.querySelector(".dropdown-menu")
+console.log(navSvgSec);
+
+// navSvg.onclick =  () => {
+//   // notifications.classList.toggle("show")
+//   console.log("red");
+// }
+
+navSvg.onclick = show
+navSvgSec.onclick = show
+function show() {
+  console.log("good");
+  notifications.classList.toggle("show")
+}
+
+
