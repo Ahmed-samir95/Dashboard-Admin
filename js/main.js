834523ceae0f4dae03c2b67f7f4f8ce2996eb904
ahmed-samir <ahmedsamirr126@gmail.com>
2024-05-19 09:55:44 +0200

    show msg dropdown when we click on icon

--- js/main.js
+++ js/main.js
@@ -2,18 +2,15 @@
 let navSvg = document.querySelector(".prs-icon")
 let navSvgSec = document.querySelector(".prs-icon-2")
 let notifications = document.querySelector(".dropdown-menu")
-console.log(navSvgSec);
-
-// navSvg.onclick =  () => {
-//   // notifications.classList.toggle("show")
-//   console.log("red");
-// }
+let msgdropdown = document.querySelectorAll("ul")[1]
+console.log(msgdropdown);
 
 navSvg.onclick = show
-navSvgSec.onclick = show
 function show() {
   console.log("good");
   notifications.classList.toggle("show")
 }
-
-
+navSvgSec.onclick = msgdropdownMenu
+function msgdropdownMenu() {
+  msgdropdown.classList.toggle("show")
+}
