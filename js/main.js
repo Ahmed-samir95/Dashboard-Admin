86414a8de63749a41309f940ede82a23cf722303
ahmed-samir <ahmedsamirr126@gmail.com>
2024-05-20 11:55:45 +0200

    show and hide profile dropdown menu using js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 //  get nav elements
 let navSvg = document.querySelector(".prs-icon")
 let navSvgSec = document.querySelector(".prs-icon-2")
+let proileImg = document.querySelector(".prs-img")
 let notifications = document.querySelector(".dropdown-menu")
 let msgdropdown = document.querySelectorAll("ul")[1]
-console.log(msgdropdown);
+let profileDropdown = document.querySelectorAll("ul")[2]
+
+console.log(profileDropdown);
 
 navSvg.onclick = show
 function show() {
@@ -13,4 +16,8 @@ function show() {
 navSvgSec.onclick = msgdropdownMenu
 function msgdropdownMenu() {
   msgdropdown.classList.toggle("show")
+}
+proileImg.onclick = profiledropdownMenu
+function profiledropdownMenu() {
+  profileDropdown.classList.toggle("show")
 }
