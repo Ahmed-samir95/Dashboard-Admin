91de50f9645c553a99a984cbfc6863aa4dd3fbef
ahmed-samir <ahmedsamirr126@gmail.com>
2024-05-28 19:12:00 +0200

    Clear the useless part of JavaScript

--- js/main.js
+++ js/main.js
@@ -29,29 +29,6 @@ function profiledropdownMenu() {
   notifications.classList.remove("show")
 }
 // ----------------------------------------------------------------------------------------------
-//  get dashboard-content elements
-let dashboardContentHeading = document.querySelector(".dashboard-content .dashboard-menu .dashboard-item h3")
-let dashboardLinks = document.querySelector(".dashboard-content .dashboard-menu .dashboard-item ul")
-console.log(dashboardContentHeading);
-console.log(dashboardLinks);
-// dashboardContentHeading.onclick = dashboard
-// function dashboard() {
-//   // dashboardLinks.classList.toggle("show")
-//   dashboardLinks.style.display = "block"
-
-// }
-// dashboardContentHeading.addEventListener("click", () => {
-//   dashboardLinks.classList.toggle("show")
-// })
-
-dashboardContentHeading.addEventListener('click', () => {
-  if (dashboardLinks.style.display === 'none') {
-    dashboardLinks.style.display = 'block';
-    dashboardLinks.style.transition = '.5s';
-  } else {
-    dashboardLinks.style.display = 'none';
-  }
-});
 
 
 
